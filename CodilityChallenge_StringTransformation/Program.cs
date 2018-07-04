787654034e1bf6a6d899312ab4c461e13c52a2d2
mark_skidmore <mark.skidmore@autopoint.com>
2018-07-03 17:17:15 -0700

    Refactored static code into class StringTransformer.

--- CodilityChallenge_StringTransformation/Program.cs
+++ CodilityChallenge_StringTransformation/Program.cs
@@ -7,34 +7,15 @@ using System.Threading.Tasks;
 namespace CodilityChallenge_StringTransformation
 {
     class Program
-    {
-        public static string solution(string S)
-        {
-            return GetReplace(S);
-        }
-
-        public static string GetReplace(string input)
-        {
-            /*string retval = input.Replace("AB", "AA")
-                                 .Replace("BA", "AA")
-                                 .Replace("CB", "CC")
-                                 .Replace("BC", "CC")
-                                 .Replace("AA", "A")
-                                 .Replace("CC", "C");*/
-
-            string retval = input.Replace("AA", "")
-                                 .Replace("BB", "")
-                                 .Replace("CC", "");
-
-            if (input == retval)
-                return retval;
-            else
-                return GetReplace(retval);
-        }
+    {        
         static void Main(string[] args)
         {
-            string S = "ACCAABBC";
-            string result = solution(S);
+            //string S = "ACCAABBC"; // Returns "AC"
+            //string S = "ABCBBCBA"; // Returns empty string
+            string S = "BABABA";     // Returns "BABABA"
+
+            StringTransformer st = new StringTransformer();
+            string result = st.Solution(S);
 
             Console.WriteLine("Result: " + result);
             Console.ReadKey();
---
+++ CodilityChallenge_StringTransformation/StringTransformer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityChallenge_StringTransformation
+{
+    class StringTransformer
+    {
+        public string Solution(string S)
+        {
+            if (S.Length > 50000)
+                return "";
+
+            return GetReplace(S);
+        }
+
+        public string GetReplace(string input)
+        {
+            /*string retval = input.Replace("AB", "AA")
+                                 .Replace("BA", "AA")
+                                 .Replace("CB", "CC")
+                                 .Replace("BC", "CC")
+                                 .Replace("AA", "A")
+                                 .Replace("CC", "C");*/
+
+            string retval = input.Replace("AA", "")
+                                 .Replace("BB", "")
+                                 .Replace("CC", "");
+
+            if (input == retval)
+                return retval;
+            else
+                return GetReplace(retval);
+        }
+    }
+}
