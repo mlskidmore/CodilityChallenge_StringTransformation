7f5230823acd6f1281240fec0099297bd53d2352
mark_skidmore <mark.skidmore@autopoint.com>
2018-07-03 17:02:01 -0700

    Initial check-in

--- CodilityChallenge_StringTransformation/Program.cs
+++ CodilityChallenge_StringTransformation/Program.cs
@@ -8,9 +8,35 @@ namespace CodilityChallenge_StringTransformation
 {
     class Program
     {
+        public static string solution(string S)
+        {
+            return GetReplace(S);
+        }
+
+        public static string GetReplace(string input)
+        {
+            /*string retval = input.Replace("AB", "AA")
+                                 .Replace("BA", "AA")
+                                 .Replace("CB", "CC")
+                                 .Replace("BC", "CC")
+                                 .Replace("AA", "A")
+                                 .Replace("CC", "C");*/
+
+            string retval = input.Replace("AA", "")
+                                 .Replace("BB", "")
+                                 .Replace("CC", "");
+
+            if (input == retval)
+                return retval;
+            else
+                return GetReplace(retval);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string S = "ACCAABBC";
+            string result = solution(S);
+
+            Console.WriteLine("Result: " + result);
             Console.ReadKey();
         }
     }
